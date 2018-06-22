673387383ebd1369cf82e19274b758347142bc5a
schen2-Quest <sophy.chen@quest.com>
2018-06-22 16:15:12 +0800

    install python

---
+++ WebUiTestt/installpython.ps1
@@ -0,0 +1,15 @@
+﻿$version = "3.6.4"
+ 
+$url = "https://www.python.org/ftp/python/$version/python-$version.exe"
+ 
+$python = "C:\python.exe"
+ 
+$wc = New-Object System.Net.WebClient
+ 
+$wc.DownloadFile($url, $python)
+ 
+Start-Process "C:\python.exe"  -WorkingDirectory "C:\" -ArgumentList '/s', '/v', '/qn' -Wait
+ 
+$pathInstall = "C:\Users\<user>\AppData\Local\Programs\Python\Python36-32"
+ 
+[Environment]::SetEnvironmentVariable("Path", "$env:Path;$pathInstall\;$pathInstall\Scripts\", "User")
