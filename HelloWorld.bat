d740b36b495948157b68c6667bf6f31996507b73
PROD\schen2 <sophy.chen@quest.com>
2018-08-03 11:31:46 +0800

    no message

--- HelloWorld.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd C:\Users\schen2\Desktop\python Examples
-python hello world.py
--- WebUiTestt/test_case/htmlreport.py
+++
@@ -1,9 +0,0 @@
-import HTMLTestRunner
-
-
-def save_file():
-    file_path = "pyResult.html"
-    print("--------------------------->")
-    fp = open(file_path, 'wb')
-    runner = HTMLTestRunner.HTMLTestRunner(stream=fp, title='PythonWebUiTestReport', description= 'test result as below')
-    return runner
